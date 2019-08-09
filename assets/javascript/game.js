fd0567c3c514fa12dbbcb06bd9be7e92bfd686b7
bradham <bradham@users.noreply.github.com>
2019-08-08 21:30:25 -0500

    added reset function to be called when letter is guessed or guesses=0. Need letter randomizer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,12 +9,12 @@ var charGuess = document.getElementById("charGuess");
 var wins = 0;
 var losses = 0;
 var guesses = 9;
-winCount.innerHTML = wins;
+winCount.innerHTML += ("  " + wins);
 lossCount.innerHTML = losses;
 guessCount.innerHTML = guesses;
 
 // Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
     // Captures the key press, converts it to lowercase, and saves it to a variable.
     var userLetter = event.key.toLowerCase();
@@ -25,28 +25,35 @@ document.onkeyup = function(event) {
 
     // If the letter matches the random letter then run the following functions/methods.
     if (userLetter === comLetter) {
-      //add 1 win and then write it in the winCount div
-      wins++;
-      winCount.innerHTML = wins;
-      //decrement guesses in this round
-      guesses--;
-      guessCount.innerHTML = guesses;
+        //add 1 win and then write it in the winCount div
+        wins++;
+        winCount.innerHTML = wins;
+        //decrement guesses in this round
+        resetGame();
     }
     else {
-        //add losses
-        losses++;
-        lossCount.innerHTML = losses;
+        //If guess is wrong then update guesses
         guesses--;
         guessCount.innerHTML = guesses;
-  
+
+        //Show the letter guessed
+        charGuess.innerHTML += (userLetter + ", ");
+
+        //when guesses are done record a loss and reset game
+        if (guesses == 0) {
+            losses++;
+            lossCount.innerHTML = losses;
+            resetGame();
+        };
+
+
     };
 
-    if (guesses == 0) {
-        guesses = 9;
-        guessCount.innerHTML = guesses;
-    };
+}
 
-    //add total guesses
-    charGuess.innerHTML += (userLetter + ", ");
-
+//function to reset game.
+function resetGame() {
+    guesses = 9;
+    guessCount.innerHTML = guesses;
+    charGuess.innerHTML = " ";
 }
